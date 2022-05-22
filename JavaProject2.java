919772b119596eac25f3ded06e492770ce296b68
unknown <you@example.com>
2022-05-22 19:31:08 +0530

    Two files Commited

--- JavaProject2.java
+++ JavaProject2.java
@@ -4,6 +4,9 @@ public static void main(String [] args){
 
 System.out.println("adding files to githhub");
 
+System.out.println("modifications in the program");
+
+
 }
 
 }
---
+++ JavaProject3.java
@@ -0,0 +1,9 @@
+public class JavaProject2{
+
+public static void main(String [] args){
+
+System.out.println("commit files form Local repository to Remote reposity");
+
+}
+
+}
