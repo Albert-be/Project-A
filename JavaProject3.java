66b0388a68bfc0bf6220323479f298f2a67881f4
unknown <you@example.com>
2022-05-22 20:13:24 +0530

    Two files Modified

--- JavaProject3.java
+++ JavaProject3.java
@@ -2,7 +2,7 @@ public class JavaProject2{
 
 public static void main(String [] args){
 
-System.out.println("commit files form Local repository to Remote reposity");
+System.out.println("commit files form Local repository to Remote reposities");
 
 }
 
