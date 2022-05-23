acdcb84dc8adc4bda235e762b25ad45d0eeb778c
unknown <you@example.com>
2022-05-23 18:09:40 +0530

    CR-122 changes

--- JavaProject2.java
+++ JavaProject2.java
@@ -2,7 +2,9 @@ public class JavaProject2{
 
 public static void main(String [] args){
 
-System.out.println("adding files to githhub");
+//System.out.println("adding files to githhub");
+
+	int i = 10;
 
 System.out.println("modifications done in the programs");
 
